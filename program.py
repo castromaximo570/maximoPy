8aa050e064fecc52df631afbe592cea1df7faeaf
castromaximo570 <castromaximo570@gmail.com>
2026-04-13 11:05:50 -0400

    primercommit

---
+++ program.py
@@ -0,0 +1,9 @@
+
+print ("Ingrese su contraseña y su pin")
+passw=input("Ingrese contraseña:")
+pin=input("Ingrese su pin")
+
+if passw=="temu" and pin =="3435":
+    print("Bienvenido al sistema")
+else:
+    print("Acceso denegado")
